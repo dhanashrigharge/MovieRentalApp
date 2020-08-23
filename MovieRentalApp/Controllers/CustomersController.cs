b0246fedde236fbfc23d4623d4a0cac9c33b8e09
Dhanashri Gharge <dhanashrigharge1997@gmail.com>
2020-08-23 17:56:07 +0530

    Display Customers with Membership Types.

---
+++ MovieRentalApp/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Ajax.Utilities;
+using MovieRentalApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace MovieRentalApp.Controllers
+{
+   
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context ;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult ViewCustomers()
+        {
+            var customers = _context.Customers.Include(m => m.MembershipType).ToList();
+            return View(customers);
+        }
+        
+    }
+}
