fd3bd0c57836d4990fe0ccbfb6db1f0055b443b2
Dhanashri Gharge <dhanashrigharge1997@gmail.com>
2020-08-30 16:11:13 +0530

    Building and Testing Web API.

---
+++ MovieRentalApp/Controllers/API/CustomersController.cs
@@ -0,0 +1,77 @@
+﻿using MovieRentalApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+
+namespace MovieRentalApp.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: api/Customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.Include(m => m.MembershipType).ToList();
+        }
+
+        // GET: api/Customers/5
+        public Customer GetCustomer(int id)
+        {          
+            var customer = _context.Customers.Include(m => m.MembershipType).SingleOrDefault(c =>c.Id==id);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return customer;
+        }
+
+        [HttpPost]
+        // POST: api/Customers
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return customer;
+        }
+
+        [HttpPut]
+        // PUT: api/Customers/5
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id==id);
+            if (customerInDb==null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            customerInDb.Name = customer.Name;
+            customerInDb.DateOfBirth = customer.DateOfBirth;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            _context.SaveChanges();
+        }
+
+        [HttpDelete]
+        // DELETE: api/Customers/5
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ MovieRentalApp/Controllers/API/MoviesController.cs
@@ -0,0 +1,74 @@
+﻿using MovieRentalApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Runtime.InteropServices;
+
+namespace MovieRentalApp.Controllers.API
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.Include(g => g.Genre).ToList();
+        }
+
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.Include(g => g.Genre).SingleOrDefault(m => m.Id==id);
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return movie;
+        }
+
+        [HttpPost]
+        public Movie CreateMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return movie;
+        }
+
+        [HttpPut]
+        public void UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id==id);
+            if(movieInDb==null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            movieInDb.Name = movie.Name;
+            movieInDb.ReleaseDate = movie.ReleaseDate;
+            movieInDb.DateAdded = movie.DateAdded;
+            movieInDb.GenreId = movie.GenreId;
+            movieInDb.NumberInStock = movie.NumberInStock;
+            _context.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+        }
+    }
+}
